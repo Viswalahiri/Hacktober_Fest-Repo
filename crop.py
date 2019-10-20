09b38e28bc2563577944effe4337ff6c52f29338
Manasa Maganti <manasamaganti99@gmail.com>
2019-10-20 09:37:25 +0530

    Added crop.py

---
+++ crop.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as pd
+img=cv2.imread('bike32.jpg')
+height, width=img.shape[:2]
+
+start_row, start_col=int(height*.25), int(width*.25)
+
+end_row, end_col=int(height*.75), int(width*.75)
+
+cropped= img[start_row:end_row, start_col:end_col]
+
+cv2.imshow('Original', img)
+cv2.waitKey(0)
+
+cv2.imshow('Cropped', cropped)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
